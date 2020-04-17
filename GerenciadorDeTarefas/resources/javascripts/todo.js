240cf2e8797cee2fce73c62196fa0c464b29aa79
Rayssa Freitas <rayssafreitas13@gmail.com>
2020-04-16 23:31:54 -0300

    exclusão de tarefa e inicio da alteração de tarefa

--- GerenciadorDeTarefas/resources/javascripts/todo.js
+++ GerenciadorDeTarefas/resources/javascripts/todo.js
@@ -5,6 +5,26 @@ $(function(){
 		}));
 	}
 
+	function onTarefaItemClick(){
+		var text = $(this).children('.tarefa-texto').text();
+		var html = "<input type='text' " + "class='tarefa-edit' value='" + text + "'>";
+		$(this).html(html);
+	}
+
+	function onTarefaEditKeydown(event){
+		if(event.which === 13){
+			savePendingEdition($(this));
+		}
+	}
+
+	function savePendingEdition(tarefa){
+		console.log("é aqui que vamos salvar nossa tarefa");
+	}
+
 
 	$(".tarefa-delete").click(onTarefaDeleteClick);
+
+	$(".tarefa-item").click(onTarefaItemClick);
+
+	$(".tarefa-edit").keydown(onTarefaEditKeydown)
 });
