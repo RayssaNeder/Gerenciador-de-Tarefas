f33a2c115361c98ac1d9cd708201a7ea7dd2e284
Rayssa Freitas <rayssafreitas13@gmail.com>
2020-04-19 16:57:31 -0300

    Conclusão do cadastro de tarefa

--- GerenciadorDeTarefas/resources/javascripts/todo.js
+++ GerenciadorDeTarefas/resources/javascripts/todo.js
@@ -1,30 +1,80 @@
 $(function(){
+
+	var $lastClicked;
+
 	function onTarefaDeleteClick(){
-		console.log($(this).parent(".tarefa-item").hide('slow', function(){
+		console.log($(this).parent(".tarefa-item").off('click').hide('slow', function(){
 			$(this).remove();
 		}));
 	}
 
 	function onTarefaItemClick(){
-		var text = $(this).children('.tarefa-texto').text();
-		var html = "<input type='text' " + "class='tarefa-edit' value='" + text + "'>";
-		$(this).html(html);
+
+		if(!$(this).is($lastClicked)){
+			if($lastClicked !== undefined){
+				savePendingEdition($lastClicked);
+			}
+
+			$lastClicked = $(this);
+		
+			var text = $lastClicked.children('.tarefa-texto').text();
+			var html = "<input type='text' " + "class='tarefa-edit' value='" + text + "'>";
+			$lastClicked.html(html);
+
+			$(".tarefa-edit").keydown(onTarefaEditKeydown);
+		}  
 	}
 
 	function onTarefaEditKeydown(event){
 		if(event.which === 13){
-			savePendingEdition($(this));
+			savePendingEdition($lastClicked);
+			$lastClicked = undefined;
 		}
 	}
 
-	function savePendingEdition(tarefa){
-		console.log("é aqui que vamos salvar nossa tarefa");
+	function savePendingEdition($tarefa){
+		var text = $tarefa.children('.tarefa-edit').val();
+		$tarefa.empty();
+		$tarefa.append("<div class='tarefa-texto'>" + text + "</div>")
+		.append("<div class='tarefa-delete'></div>")
+		.append("<div class='clear'></div>");
+
+		$(".tarefa-delete").click(onTarefaDeleteClick);
+
+		$tarefa.click(onTarefaItemClick)
 	}
+
+	function onTarefaKeydown(event){
+		if(event.which === 13){
+			addTarefa($("#tarefa").val());
+
+		}
+	}
+
+	function addTarefa(text){
+		var $tarefa = $("<div />")
+						.addClass("tarefa-item")
+						.append($("<div />)")
+							.addClass("tarefa-texto")
+							.text(text))
+						.append($("<div />")
+							.addClass("tarefa-delete"))
+						.append($("<div />")
+							.addClass("clear"));
+
+		$("#tarefa-lista").append($tarefa);
+		$(".tarefa-delete").click(onTarefaDeleteClick);
+		$(".tarefa-item").click(onTarefaItemClick);
+	}
+
+	
 
 
 	$(".tarefa-delete").click(onTarefaDeleteClick);
 
 	$(".tarefa-item").click(onTarefaItemClick);
 
-	$(".tarefa-edit").keydown(onTarefaEditKeydown)
+	$(".tarefa-edit").keydown(onTarefaEditKeydown);
+
+	$('#tarefa').keydown(onTarefaKeydown);
 });
